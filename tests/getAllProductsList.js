b652d3f69f604ae77adefbad717eec90de48ebb9
Max <maksimhakobjanyan@gmail.com>
2024-07-25 23:15:53 +0400

    Update getAllProductsList.js

--- tests/getAllProductsList.js
+++ tests/getAllProductsList.js
@@ -1,75 +1,60 @@
 const {getAllProducts} = require("../requests");
+const {productsArraySchema} = require("../joiSchemas/validationForAllProducts");
 
-describe('API Tests for Products List', () => {
+describe('API Tests for Products List - Positive Test Cases', () => {
     let response = {}
+    let products = []
+
     beforeAll(async () => {
         response = await getAllProducts();
-    }, 2000)
-    test('Valid Request - Status 200 and Data Structure', async () => {
+        products = response.data['products']
+    }, 1000) // Info: Timeout will check the following test case: "Check response time"
+
+    test('Check status code to be 200', async () => {
         expect(response.status).toBe(200);
         expect(response.data).toHaveProperty('products');
-        expect(Array.isArray(response.data.products)).toBe(true);
     });
 
-    test('Product Data Structure', async () => {
-        const products = response.data.products;
-        products.forEach(product => {
-            expect(product).toHaveProperty('id');
-            expect(product).toHaveProperty('name');
-            expect(product).toHaveProperty('category');
-            expect(product).toHaveProperty('price');
-        });
-    });
-
-    test('Non-Empty Product List', async () => {
-        const products = response.data.products;
+    test('Check that response is not empty', async () => {
         expect(products.length).toBeGreaterThan(0);
     });
 
-    test('Data Types of Product Fields', async () => {
-        const products = response.data.products;
-        products.forEach(product => {
-            expect(typeof product.id).toBe('number');
-            expect(typeof product.name).toBe('string');
-            expect(typeof product.category).toBe('object');
-            expect(typeof product.price).toBe('string');
-        });
+    test('Check response json schema', async () => {
+        const validation = productsArraySchema.validate(products);
+        if (validation.error) {
+            console.error(validation.error.details);
+        }
     });
 
-    test('Incorrect HTTP Method - Status 405', async () => {
-        const response = await getAllProducts("POST")
-        expect(response.data.responseCode).toBe(405)
-        expect(response.data.message).toBe("This request method is not supported.")
-    });
-
-    test.skip('Content-Type Header', async () => {
-        const response = await getAllProducts();
-        expect(response.headers['content-type']).toMatch(/application\/json/);
-    });
-
-    test('Consistency in Product IDs', async () => {
-        const response = await getAllProducts();
-        const products = response.data.products;
+    test('Check that product IDs are unique', async () => {
         const ids = products.map(product => product.id);
         const uniqueIds = new Set(ids);
         expect(ids.length).toBe(uniqueIds.size);
     });
 
-    test('Handling of Optional Fields', async () => {
-        const response = await getAllProducts();
-        const products = response.data.products;
-        products.forEach(product => {
-            // Assuming 'description' is an optional field
-            if (product.hasOwnProperty('description')) {
-                expect(typeof product.description).toBe('string');
-            }
-        });
-    });
-
-    test('Data Consistency Over Multiple Requests', async () => {
-        const response1 = await getAllProducts();
+    test('Check that the response is same each time', async () => {
         const response2 = await getAllProducts();
-        expect(response1.data).toEqual(response2.data);
+        expect(response.data).toEqual(response2.data);
+    });
+});
+
+describe('API Tests for Products List - Negative Test Cases', () => {
+    test('Incorrect HTTP Method - Status 405', async () => {
+        const response = await getAllProducts("POST")
+        expect(response.data['responseCode']).toBe(405)
+        expect(response.data.message).toBe("This request method is not supported.")
     });
 
-});
+    test('Check the URL with multiple slashes', async () => {
+        // In this case, no matter how many slashes there are, the response is 200, so the case will fail
+        const response = await getAllProducts("GET", '/////api//productsList')
+        expect(response.status).toBe(404);
+        expect(response.statusText).toBe("Not Found");
+    });
+
+    test('Check the URL with wrong endpoint', async () => {
+        const response = await getAllProducts("GET", '/api/productsListt')
+        expect(response.status).toBe(404);
+        expect(response.statusText).toBe("Not Found");
+    });
+})
